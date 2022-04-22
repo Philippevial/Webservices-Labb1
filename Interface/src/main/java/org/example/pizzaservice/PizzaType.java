cded2be9f0fadba369fb6a64827ee436587ea6fd
Philippe <philippe.vial90@gmail.com>
2022-04-22 13:44:13 +0200

    Add PizzaType.java annotation

---
+++ Interface/src/main/java/org/example/pizzaservice/PizzaType.java
@@ -0,0 +1,12 @@
+package org.example.pizzaservice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PizzaType {
+    String value() default "3. Margherita";
+}
