205387063b7e1e2c315dafed505982c97624b56a
Philippe <philippe.vial90@gmail.com>
2022-04-22 13:44:44 +0200

    Add PizzaType.java annotation to pizza classes

--- Provider/src/main/java/org/example/pizzaprovider/HawaiiPizza.java
+++ Provider/src/main/java/org/example/pizzaprovider/HawaiiPizza.java
@@ -1,11 +1,13 @@
 package org.example.pizzaprovider;
 
 import org.example.pizzaservice.PizzaMaker;
+import org.example.pizzaservice.PizzaType;
 
+@PizzaType("2. Hawaii")
 public class HawaiiPizza implements PizzaMaker {
 
     @Override
     public String pizzaOrder() {
-        return "Pizza order: HawaiiPizza \n HawaiiPizza on the way!";
+        return "Pizza order: Hawaii \n Hawaii on the way!\n";
     }
 }
--- Provider/src/main/java/org/example/pizzaprovider/KebabPizza.java
+++ Provider/src/main/java/org/example/pizzaprovider/KebabPizza.java
@@ -1,11 +1,13 @@
 package org.example.pizzaprovider;
 
 import org.example.pizzaservice.PizzaMaker;
+import org.example.pizzaservice.PizzaType;
 
+@PizzaType("1. KebabPizza")
 public class KebabPizza implements PizzaMaker {
 
     @Override
     public String pizzaOrder() {
-        return "Pizza order: Kebabpizza \n Kebabpizza on the way!";
+        return "Pizza order: Kebabpizza \n Kebabpizza on the way!\n";
     }
 }
